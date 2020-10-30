0e6e90ff1990fa932bae616a72ac33d313621a22
Hu Keping <hukeping@huawei.com>
2020-10-30 11:37:14 +0000

    Add support for mercurial and gmp as first mercurial project
    Signed-off-by: Hu Keping <hukeping@huawei.com>

--- validate_access.bash
+++ validate_access.bash
@@ -11,21 +11,38 @@ unshallow_list=(
 	"https://git.netfilter.org/iptables"
 )
 
+Mercurial_list=(
+	"https://gmplib.org/repo/gmp"
+)
+
 declare -A unshallow_array
+declare -A Mercurial_array
 
 for i in ${unshallow_list[*]}
 do
 	unshallow_array[$i]=1
 done
 
+for i in ${Mercurial_list[*]}
+do
+	Mercurial_array[$i]=1
+done
+
 for i in $(cat upstream.yaml | grep "upstream: " | awk '{print $NF}')
 do
 	echo "Sync $i ..."
+	repo_name=$(echo $i | awk -F/ '{print $NF}')
+
+	if [[ ${Mercurial_array[$i]} ]]
+	then
+		hg -q clone $i $repo_name
+		continue
+	fi
 
 	if [[ ${unshallow_array[$i]} ]]
 	then
-		git clone -q $i
+		git clone -q $i $repo_name
 	else
-		git clone -q --depth 1 $i
+		git clone -q --depth 1 $i $repo_name
 	fi
 done
